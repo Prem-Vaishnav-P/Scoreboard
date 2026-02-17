00abc54e8676ef0f1aa2b1b3106170ff41e966da
Prem-Vaishnav-P <premvaishnav9101@gmail.com>
2026-02-17 10:39:23 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let homeEl = document.getElementById("home-el")
+let guestEl = document.getElementById("guest-el")
+homeEl.textContent = 0
+guestEl.textContent = 0
+let homePoints = 0
+let guestPoints = 0
+function increment1PointHome(){
+    homePoints+=1
+    homeEl.textContent = homePoints
+}
+function increment2PointHome(){
+    homePoints+=2
+    homeEl.textContent = homePoints
+}
+function increment3PointHome(){
+    homePoints+=3
+    homeEl.textContent = homePoints
+}
+function increment1PointGuest(){
+    guestPoints+=1
+    guestEl.textContent = guestPoints
+}
+function increment2PointGuest(){
+    guestPoints+=2
+    guestEl.textContent = guestPoints
+}
+function increment3PointGuest(){
+    guestPoints+=3
+    guestEl.textContent = guestPoints
+}
+function reset(){
+    homePoints = 0
+    guestPoints = 0
+    homeEl.textContent = 0
+    guestEl.textContent = 0
+}
